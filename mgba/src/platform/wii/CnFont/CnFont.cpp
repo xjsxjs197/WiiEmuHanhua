775a6951bd49a9cdbde18cf75713c81ca16eb1f5
xjsxjs197 <31617363+xjsxjs197@users.noreply.github.com>
2018-11-27 09:11:18 +0800

    xjsxjs197
    gbafont ia8 -> i4

--- mgba/src/platform/wii/CnFont/CnFont.cpp
+++ mgba/src/platform/wii/CnFont/CnFont.cpp
@@ -10,14 +10,14 @@
 #include <fat.h>
 #include <malloc.h>
 
-#include "mGba_CnFont_IA8_dat.h"
+#include "mGba_CnFont_I4_dat.h"
 #include "CnFont.h"
 
-#define CH_FONT_HEIGHT 24
-#define CH_FONT_WIDTH 24
+#define CH_FONT_HEIGHT 32
+#define CH_FONT_WIDTH 32
 
 // 字符图片的大小
-#define CHAR_IMG_SIZE CH_FONT_HEIGHT * CH_FONT_WIDTH * 2
+#define CHAR_IMG_SIZE CH_FONT_HEIGHT * CH_FONT_WIDTH / 2
 
 // 保存字符图片映射信息
 static std::map<uint16_t, int> charWidthMap;
@@ -27,11 +27,11 @@ static std::map<uint16_t, u8*> charImgBufMap;
 * 初始化
 */
 void InitCnFont() {
-	int searchLen = (int)(mGba_CnFont_IA8_dat_size / (CHAR_IMG_SIZE + 4));
+	int searchLen = (int)(mGba_CnFont_I4_dat_size / (CHAR_IMG_SIZE + 4));
 	int bufIndex = 0;
 	int skipSetp = (CHAR_IMG_SIZE + 4) / 2;
 
-	uint16_t *zhFontBufTemp = (uint16_t *)mGba_CnFont_IA8_dat;
+	uint16_t *zhFontBufTemp = (uint16_t *)mGba_CnFont_I4_dat;
     while (bufIndex < searchLen)
     {
         charWidthMap.insert(std::pair<uint16_t, int>(*zhFontBufTemp, *((u8*)(zhFontBufTemp + 1) + 1)));
@@ -75,7 +75,20 @@ void DrawCnChar(int x, int y, uint32_t color, uint16_t glyph) {
 	GX_SetBlendMode(GX_BM_BLEND, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA, GX_LO_NOOP);
 	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
 
-	GX_InitTexObj(&fontTexObj, (u8*)(charImgBufMap[glyph]), CH_FONT_WIDTH, CH_FONT_HEIGHT, GX_TF_IA8, GX_CLAMP, GX_CLAMP, GX_FALSE);
+    std::map<uint16_t, u8*>::iterator iter;
+    u8* charPngBug;
+    iter = charImgBufMap.find(glyph);
+    if (iter != charImgBufMap.end())
+    {
+        charPngBug = iter->second;
+    }
+    else
+    {
+        charPngBug = (u8*)(charImgBufMap[0]);
+    }
+
+
+	GX_InitTexObj(&fontTexObj, charPngBug, CH_FONT_WIDTH, CH_FONT_HEIGHT, GX_TF_I4, GX_CLAMP, GX_CLAMP, GX_FALSE);
     GX_LoadTexObj(&fontTexObj, GX_TEXMAP0);
 
     GX_Begin(GX_QUADS, GX_VTXFMT0, 4);
@@ -110,5 +123,17 @@ int GetCharHeight(uint16_t glyph) {
 * 取得字符宽度
 */
 int GetCharWidth(uint16_t glyph) {
+    std::map<uint16_t, int>::iterator iter;
+    int charPngWidth;
+    iter = charWidthMap.find(glyph);
+    if (iter != charWidthMap.end())
+    {
+        charPngWidth = iter->second;
+    }
+    else
+    {
+        charPngWidth = charWidthMap[0];
+    }
+
     return charWidthMap[glyph];
 }
